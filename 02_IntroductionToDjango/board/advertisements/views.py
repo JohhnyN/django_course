7b01ee7b014196e3acdedd8b331e25dc60e1a416
JohnnyN <kazpiter@gmail.com>
2022-05-20 12:39:39 +0600

    Данные в шаблон передаю из функции

--- 02_IntroductionToDjango/board/advertisements/views.py
+++ 02_IntroductionToDjango/board/advertisements/views.py
@@ -5,23 +5,65 @@ from django.shortcuts import render
 
 
 def advertisement_list(request, *args, **kwargs):
-    return render(request, 'advertisement/advertisement_list.html', {})
+    course_pages = [{'title': 'Старт в DevOps: системное администрирование для начинающих', 'url_name': 'devops'},
+                    {'title': 'Веб-дизайн 3.0', 'url_name': 'web-design-3'},
+                    {'title': 'Управляющий рестораном', 'url_name': 'restaurant-manager'},
+                    {'title': 'Excel + Google Таблицы с нуля до PRO', 'url_name': 'excel-gsheets'},
+                    {'title': 'Музыкальный менеджер', 'url_name': 'music-manager'}]
+    return render(request, 'advertisement/advertisement_list.html',
+                  {'title': 'advertisement_list', 'course_pages': course_pages})
 
 
 def web_design_3(request, *args, **kwargs):
-    return render(request, 'advertisement/web-design-3.html', {})
+    context = {
+        'title': 'web-design-3',
+        'img_url': 'https://248006.selcdn.ru/LandGen/desktop_2_262ebbb0b9720b2314fe4d9ce45cc3609e942716.webp',
+        'link_to_the_course': 'https://skillbox.ru/course/web-design-3/',
+        'course_name': 'Веб-дизайн 3.0',
+        'price': '102 960'
+    }
+    return render(request, 'advertisement/devops.html', context)
 
 
 def devops(request, *args, **kwargs):
-    return render(request, 'advertisement/devops.html', {})
+    context = {
+        'title': 'devops',
+        'img_url': 'https://248006.selcdn.ru/LandGen/desktop_2_bd5c75285fc4a318ae23886b16f51b2b110ea1b1.webp',
+        'link_to_the_course': 'https://skillbox.ru/course/devops/',
+        'course_name': 'Старт в DevOps',
+        'price': '4 514'
+    }
+    return render(request, 'advertisement/devops.html', context)
 
 
 def restaurant_manager(request, *args, **kwargs):
-    return render(request, 'advertisement/restaurant-manager.html', {})
+    context = {
+        'title': 'restaurant-manager',
+        'img_url': 'https://248006.selcdn.ru/LandGen/desktop_2_70fe1c9221c549f772e76482718b4c3d663dcfe6.webp',
+        'link_to_the_course': 'https://skillbox.ru/course/restaurant-manager/',
+        'course_name': 'Управляющий рестораном',
+        'price': '68 345'
+    }
+    return render(request, 'advertisement/restaurant-manager.html', context)
 
 
 def excel_gsheets(request, *args, **kwargs):
-    return render(request, 'advertisement/excel-gsheets.html', {})
+    context = {
+        'title': 'excel-gsheets',
+        'img_url': 'https://248006.selcdn.ru/LandGen/desktop_2_b609db3df74c3b33f9819476d65eef1ed431af6f.webp',
+        'link_to_the_course': 'https://skillbox.ru/course/excel-gsheets/',
+        'course_name': 'Excel + Google Таблицы с нуля до PRO',
+        'price': '31 597'
+    }
+    return render(request, 'advertisement/restaurant-manager.html', context)
+
 
 def music_manager(request, *args, **kwargs):
-    return render(request, 'advertisement/music-manager.html', {})
+    context = {
+        'title': 'music-manager',
+        'img_url': 'https://248006.selcdn.ru/LandGen/desktop_2_4280e43b6ab85f950b81ec1f4be9d2170943f145.webp',
+        'link_to_the_course': 'https://skillbox.ru/course/music-manager/',
+        'course_name': 'Музыкальный менеджер',
+        'price': '5 382'
+    }
+    return render(request, 'advertisement/restaurant-manager.html', context)
